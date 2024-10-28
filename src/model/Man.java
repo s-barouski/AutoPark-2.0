13d7aa30f6982178bdc3082310e73f75602cc452
s-barouski <s.barouski@mail.ru>
2024-10-28 19:47:18 +0300

    Revert "add maven"
    This reverts commit e2fe9b41

---
+++ src/model/Man.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Man extends Driver {
+    private int footSize;
+
+    public void setFootSize(int footSize) {
+        this.footSize = footSize;
+    }
+
+    public int getFootSize() {
+
+        return footSize;
+    }
+
+
+}
